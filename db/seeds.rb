9de905f9427e2f00699d5681bff78714238fe54e
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-01-25 22:38:59 +0100

    added seed default messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Greeting.create(
+  [
+    { message: 'Hello! Good morning!' },
+    { message: 'Ututu Oma!' },
+    { message: 'Olá! Bom dia!' },
+    { message: '¡Hola! ¡Buenos días!' },
+    { message: 'Ciao! Buon giorno!' }
+  ]
+)
